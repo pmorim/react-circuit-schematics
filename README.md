97b2a61763c800781ac75bb801fb148d6dbee499
Pedro Morim <1180798@isep.ipp.pt>
2021-06-24 21:17:29 +0100

    Added some basic Node logic

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 > A React library that allows you to draw circuit schematics
 
 [![NPM](https://img.shields.io/npm/v/react-draw-circuits.svg)](https://www.npmjs.com/package/react-draw-circuits) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
-
 [![Netlify Status](https://api.netlify.com/api/v1/badges/4b893982-6105-499c-971e-a42eb0797b37/deploy-status)](https://app.netlify.com/sites/react-draw-circuits/deploys)
 
 ## Install
--- src/components/Ports/Port.jsx
+++ src/components/Ports/Port.jsx
@@ -3,30 +3,39 @@ import PropTypes from 'prop-types';
 
 import styles from './Port.module.css';
 
-export const Port = forwardRef(({ radius, position, bounds, ...rest }, ref) => {
-  return (
-    <div
-      ref={ref}
-      className={styles.port}
-      style={{
-        // The size of the port
-        width: radius * 2,
-        height: radius * 2,
+export const Port = forwardRef(
+  ({ radius, color, position, bounds, ...rest }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={styles.port}
+        style={{
+          // The size of the port
+          width: radius * 2,
+          height: radius * 2,
 
-        // The positioning of the port
-        left: `calc(${position.x * bounds.x}px - ${radius}px)`,
-        top: `calc(${position.y * bounds.y}px - ${radius}px)`,
-      }}
-      {...rest}
-    />
-  );
-});
+          // The coloring
+          backgroundColor: color,
+
+          // The positioning of the port
+          left: `calc(${position.x * bounds.x}px - ${radius}px)`,
+          top: `calc(${position.y * bounds.y}px - ${radius}px)`,
+        }}
+        {...rest}
+      />
+    );
+  },
+);
 
 Port.propTypes = {
   /**
    * The radius of a port
    */
   radius: PropTypes.number,
+  /**
+   * The color of the port
+   */
+  color: PropTypes.color,
   /**
    * The relative position of the port
    */
@@ -39,6 +48,7 @@ Port.propTypes = {
 
 Port.defaultProps = {
   radius: 10,
+  color: '#bbb',
   position: { x: 0, y: 0 },
   bounds: { x: 10, y: 10 },
 };
