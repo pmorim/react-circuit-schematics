9d711a4a3b8b5b8313497f3fa8b9451f0edc4b6a
Pedro Morim <1180798@isep.ipp.pt>
2021-07-10 21:15:36 +0100

    Removed unused property

--- src/schematic/state.js
+++ src/schematic/state.js
@@ -39,13 +39,11 @@ export const initialState = {
     nodes: [
       {
         id: 'a',
-        type: 'real',
         position: { x: 0, y: 0 },
         //properties: { color: '#000', radius: 10 },
       },
       {
         id: 'b',
-        type: 'real',
         position: { x: 200, y: 100 },
         //properties: { color: '#000', radius: 10 },
       },
