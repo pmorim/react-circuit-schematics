4b928b7dccb2060a020be4e726cd43b7d82d092e
Pedro Morim <1180798@isep.ipp.pt>
2021-07-23 20:48:49 +0100

    Fixed dragging hitbox problems

--- src/components/Schematic/Schematic.jsx
+++ src/components/Schematic/Schematic.jsx
@@ -46,6 +46,7 @@ export const Schematic = ({
       {...rest}
     >
       <SelectableGroup
+        style={{ width: '100%', height: '100%' }}
         clickClassName='rdc-handle'
         duringSelection={duringSelection}
         onSelectionFinish={onSelectionFinish}
