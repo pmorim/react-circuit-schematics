c7c607bab2a3d8873b5a7d104ef79643fa78f28a
Pedro Morim <1180798@isep.ipp.pt>
2021-08-05 15:33:09 +0100

    Simplified Schematic's story

--- src/stories/Basic/Schematic.stories.jsx
+++ src/stories/Basic/Schematic.stories.jsx
@@ -1,5 +1,6 @@
 import { Schematic } from '../../components/Schematic';
 import { useSchematic } from '../../hooks/useSchematic';
+import { RLC_Circuit } from './RLC_Circuit';
 
 export default {
   title: 'Basic/Schematic',
@@ -19,18 +20,18 @@ export default {
 
 export const Empty = () => <Schematic width={800} height={500} />;
 
-export const RLC_Circuit = () => {
+export const JSON_FormatExample = () => {
   const RLC_Circuit = {
     components: [
       {
         id: '8c0168ec-07fe-4b2c-96ee-4292ab34cfb2',
         type: 'DC Voltage Source',
-        position: { x: 50, y: 150 },
+        position: { x: 200, y: 150 },
         label: {
           name: 'U',
           value: '5',
           unit: 'V',
-          position: { x: 90, y: 40 },
+          position: { x: -50, y: 40 },
         },
         ports: [
           {
@@ -48,12 +49,12 @@ export const RLC_Circuit = () => {
       {
         id: 'a2475d77-22a6-47b5-b60b-04aa5af69284',
         type: 'Resistor',
-        position: { x: 200, y: 100 },
+        position: { x: 400, y: 200, angle: 90 },
         label: {
           name: 'R',
           value: '10k',
           unit: 'Ω',
-          position: { x: 10, y: 0 },
+          position: { x: 80, y: 30 },
         },
         ports: [
           {
@@ -68,79 +69,29 @@ export const RLC_Circuit = () => {
           },
         ],
       },
-      {
-        id: 'ffe5a9ee-6619-4092-8246-336621497794',
-        type: 'Inductor',
-        position: { x: 400, y: 100 },
-        label: {
-          name: 'L',
-          value: '10m',
-          unit: 'H',
-          position: { x: 10, y: 0 },
-        },
-        ports: [
-          {
-            id: '8909cf3b-f5cd-4edf-850a-37d1ac6ad9ed',
-            type: 'hybrid',
-            position: { x: 0, y: 0.5 },
-          },
-          {
-            id: 'e7bb96c9-8eae-41c5-a361-7a38836505a8',
-            type: 'hybrid',
-            position: { x: 1, y: 0.5 },
-          },
-        ],
-      },
-      {
-        id: 'ef4bc1f6-e72f-4c4c-b9bb-76ea5d36c13b',
-        type: 'Capacitor',
-        position: { x: 500, y: 200, angle: 90 },
-        label: {
-          name: 'C',
-          value: '1µ',
-          unit: 'F',
-          position: { x: 80, y: 40 },
-        },
-        ports: [
-          {
-            id: 'dc54f032-592a-4c89-90d6-0727add0908f',
-            type: 'hybrid',
-            position: { x: 0, y: 0.5 },
-          },
-          {
-            id: 'b4c2c42a-24d8-4230-8ee7-f8330926205f',
-            type: 'hybrid',
-            position: { x: 1, y: 0.5 },
-          },
-        ],
-      },
     ],
     nodes: [],
     connections: [
       {
-        id: 'da7214da-14c6-49ab-9ca6-5b2ed8a184d3',
-        start: '1f7b8784-184b-4733-ae81-53c74b62462c',
-        end: '8909cf3b-f5cd-4edf-850a-37d1ac6ad9ed',
-      },
-      {
         id: 'fd313c9d-03f2-437d-8462-331e4efc660a',
-        start: 'e7bb96c9-8eae-41c5-a361-7a38836505a8',
-        end: 'dc54f032-592a-4c89-90d6-0727add0908f',
-      },
-      {
-        id: 'aa09ec37-a2f4-4f9a-a594-710094a61b81',
-        start: 'b4c2c42a-24d8-4230-8ee7-f8330926205f',
-        end: 'c990e78e-2bc5-4afb-92ee-b8a6d1faab0f',
-      },
-      {
-        id: '7a141173-c7f0-4e4c-9120-2a363dba9f79',
         start: '7ed2507f-14fe-45e2-9246-9ee1d36eb442',
         end: 'ddd6efab-f8f2-4565-8010-1270556f1d00',
+      },
+      {
+        id: 'da7214da-14c6-49ab-9ca6-5b2ed8a184d3',
+        start: '1f7b8784-184b-4733-ae81-53c74b62462c',
+        end: 'c990e78e-2bc5-4afb-92ee-b8a6d1faab0f',
       },
     ],
   };
 
+  const { schematic } = useSchematic(RLC_Circuit);
+  return <Schematic width={800} height={500} schematic={schematic} />;
+};
+
+export const UndoAndRedo = () => {
   const { schematic, history } = useSchematic(RLC_Circuit);
+
   return (
     <>
       <button onClick={history.undo} disabled={!history.canUndo}>
@@ -150,12 +101,7 @@ export const RLC_Circuit = () => {
         Redo
       </button>
 
-      <Schematic
-        width={800}
-        height={500}
-        schematic={schematic}
-        history={history}
-      />
+      <Schematic width={800} height={500} schematic={schematic} />
     </>
   );
 };
