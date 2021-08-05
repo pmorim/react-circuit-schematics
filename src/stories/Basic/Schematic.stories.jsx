81e73b9388957d03f4509a4d1da3c738401f06ca
Pedro Morim <1180798@isep.ipp.pt>
2021-08-05 15:17:16 +0100

    Updated components ids to be more realistic

--- src/stories/Basic/Schematic.stories.jsx
+++ src/stories/Basic/Schematic.stories.jsx
@@ -23,7 +23,7 @@ export const RLC_Circuit = () => {
   const RLC_Circuit = {
     components: [
       {
-        id: 'VDC',
+        id: '8c0168ec-07fe-4b2c-96ee-4292ab34cfb2',
         type: 'DC Voltage Source',
         position: { x: 50, y: 150 },
         label: {
@@ -34,19 +34,19 @@ export const RLC_Circuit = () => {
         },
         ports: [
           {
-            id: 'VDCa',
+            id: '7ed2507f-14fe-45e2-9246-9ee1d36eb442',
             type: 'hybrid',
             position: { x: 0.5, y: 0 },
           },
           {
-            id: 'VDCb',
+            id: 'c990e78e-2bc5-4afb-92ee-b8a6d1faab0f',
             type: 'hybrid',
             position: { x: 0.5, y: 1 },
           },
         ],
       },
       {
-        id: 'R',
+        id: 'a2475d77-22a6-47b5-b60b-04aa5af69284',
         type: 'Resistor',
         position: { x: 200, y: 100 },
         label: {
@@ -57,19 +57,19 @@ export const RLC_Circuit = () => {
         },
         ports: [
           {
-            id: 'Ra',
+            id: 'ddd6efab-f8f2-4565-8010-1270556f1d00',
             type: 'hybrid',
             position: { x: 0, y: 0.5 },
           },
           {
-            id: 'Rb',
+            id: '1f7b8784-184b-4733-ae81-53c74b62462c',
             type: 'hybrid',
             position: { x: 1, y: 0.5 },
           },
         ],
       },
       {
-        id: 'L',
+        id: 'ffe5a9ee-6619-4092-8246-336621497794',
         type: 'Inductor',
         position: { x: 400, y: 100 },
         label: {
@@ -80,19 +80,19 @@ export const RLC_Circuit = () => {
         },
         ports: [
           {
-            id: 'La',
+            id: '8909cf3b-f5cd-4edf-850a-37d1ac6ad9ed',
             type: 'hybrid',
             position: { x: 0, y: 0.5 },
           },
           {
-            id: 'Lb',
+            id: 'e7bb96c9-8eae-41c5-a361-7a38836505a8',
             type: 'hybrid',
             position: { x: 1, y: 0.5 },
           },
         ],
       },
       {
-        id: 'C',
+        id: 'ef4bc1f6-e72f-4c4c-b9bb-76ea5d36c13b',
         type: 'Capacitor',
         position: { x: 500, y: 200, angle: 90 },
         label: {
@@ -103,12 +103,12 @@ export const RLC_Circuit = () => {
         },
         ports: [
           {
-            id: 'Ca',
+            id: 'dc54f032-592a-4c89-90d6-0727add0908f',
             type: 'hybrid',
             position: { x: 0, y: 0.5 },
           },
           {
-            id: 'Cb',
+            id: 'b4c2c42a-24d8-4230-8ee7-f8330926205f',
             type: 'hybrid',
             position: { x: 1, y: 0.5 },
           },
@@ -118,24 +118,24 @@ export const RLC_Circuit = () => {
     nodes: [],
     connections: [
       {
-        id: 'c1',
-        start: 'Rb',
-        end: 'La',
+        id: 'da7214da-14c6-49ab-9ca6-5b2ed8a184d3',
+        start: '1f7b8784-184b-4733-ae81-53c74b62462c',
+        end: '8909cf3b-f5cd-4edf-850a-37d1ac6ad9ed',
       },
       {
-        id: 'c2',
-        start: 'Lb',
-        end: 'Ca',
+        id: 'fd313c9d-03f2-437d-8462-331e4efc660a',
+        start: 'e7bb96c9-8eae-41c5-a361-7a38836505a8',
+        end: 'dc54f032-592a-4c89-90d6-0727add0908f',
       },
       {
-        id: 'c3',
-        start: 'Cb',
-        end: 'VDCb',
+        id: 'aa09ec37-a2f4-4f9a-a594-710094a61b81',
+        start: 'b4c2c42a-24d8-4230-8ee7-f8330926205f',
+        end: 'c990e78e-2bc5-4afb-92ee-b8a6d1faab0f',
       },
       {
-        id: 'c4',
-        start: 'VDCa',
-        end: 'Ra',
+        id: '7a141173-c7f0-4e4c-9120-2a363dba9f79',
+        start: '7ed2507f-14fe-45e2-9246-9ee1d36eb442',
+        end: 'ddd6efab-f8f2-4565-8010-1270556f1d00',
       },
     ],
   };
